33c6add6ae50b234bbeb370a4940658c7f8077ff
kiddVS <435303131@qq.com>
2017-05-25 22:23:57 +0800

    ol

---
+++ ImageUpload/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ImageUpload.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult ImageUpload()
+        {
+            HttpPostedFileBase files = Request.Files["file"];
+            if (files != null)
+            {
+                string fileName = files.FileName;
+                string extensionName = Path.GetExtension(fileName);
+                if (extensionName == ".jpg" || extensionName == ".png" || extensionName == "gif")
+                {
+                    Stream stream = files.InputStream;
+                    byte[] buffer = new byte[stream.Length];
+                    stream.Read(buffer, 0, buffer.Length);
+                    stream.Seek(0, SeekOrigin.Begin);
+                    MyImageServerEntities db = new MyImageServerEntities();
+                    var imageServersEntity = db.ImageServerInfo.Where(u => u.FlgUsable == true).ToList();
+                    Random random = new Random();
+                    int serverNum = random.Next() % imageServersEntity.Count() + 1;
+                    var uploadServer = imageServersEntity.Where(u => u.ServerId == serverNum).FirstOrDefault();
+                    WebClient client = new WebClient();
+                    string url = @"http://" + uploadServer.ServerUrl + @"/SaveFile.ashx?serverID=" + serverNum + "&ext=" + extensionName;
+                    // WebRequest.Create(url);
+                   // client.UploadData();
+                    client.UploadData(url, buffer);
+                    return Content("OK");
+                }
+                return Content("文件不是图片");
+            }
+            else
+            {
+                return Content("选择文件");
+            }
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+
+
+    }
+}
---
+++ WebServer2/SaveFile.ashx.cs
@@ -0,0 +1,47 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace WebServer2
+{
+    /// <summary>
+    /// SaveFile 的摘要说明
+    /// </summary>
+    public class SaveFile : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+
+            context.Response.ContentType = "text/plain";
+            int serverID = int.Parse(context.Request["serverID"]);
+            string ext = context.Request["ext"];
+            // HttpPostedFile file= context.Request.Files[0];
+            Stream stream = context.Request.InputStream;
+            string dir = context.Request.MapPath(@"/Image/" + DateTime.Now.ToString("yyyyMMdd"));
+            Directory.CreateDirectory(dir);
+            string fileName = @"/Image/" + DateTime.Now.ToString("yyyyMMdd") +"/"+ Guid.NewGuid() + ext;
+            using (FileStream fs = new FileStream(context.Server.MapPath(fileName), FileMode.Create))
+            {
+                stream.CopyTo(fs);
+            }
+            MyImageServerEntities db = new MyImageServerEntities();
+            ImageInfo entity = new ImageInfo() { ImageName = fileName, ImageServerId = serverID };
+            db.ImageInfo.Add(entity);
+            db.SaveChanges();
+            context.Response.Write("OK");
+            // context.Response.Write("Hello World");
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
